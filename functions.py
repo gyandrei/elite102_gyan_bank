b70629c248f342841ed46b07902cac88da4ece5e
Gyan Andrei Gabilan <gabilangyan@gmail.com>
2023-04-06 17:44:02 -0500

    prompting for name

--- functions.py
+++ functions.py
@@ -1,5 +1,20 @@
 def introduction():
-    print()
-    print("\nWelcome to Gyan Bank")
-    print()
+    print("""
+    ================================================
+    ~~~~~~~~~Welcome to Gyan Banking System~~~~~~~~~
+    ================================================  
+    """)
+
+    name = input('What is your name: ')
+
+    print(f'\nHello {name}. What would you like to do today?')
+
+    print("""
+    ================================================
+    <<< 1. New Account
+    <<< 2. Deposit
+    <<< 3. Withdraw
+    <<< 4. Schedule Meeting
+    ================================================
+    """)
 
