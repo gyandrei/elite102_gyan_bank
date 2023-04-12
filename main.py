35948063d3ce57c864b7e216b1038a13ade8dc96
Gyan Andrei Gabilan <gabilangyan@gmail.com>
2023-04-12 06:30:08 -0500

    connected mysql table to visual studio

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-import functions as md
+#import functions as md
 
-md.introduction()
+#md.introduction()
+
+
+
+import mysql.connector
+
+ 
+
+connection = mysql.connector.connect(user = 'root', database = 'example', password = 'Aquarius_9789')
+
+ 
+
+cursor = connection.cursor()
+
+ 
+
+testQuery = ('SELECT * FROM student')
+
+ 
+
+cursor.execute(testQuery)
+
+ 
+
+for item in cursor:
+
+    print(item)
+
+ 
+
+cursor.close()
+
+connection.close()
 
